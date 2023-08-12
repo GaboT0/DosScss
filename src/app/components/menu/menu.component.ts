56713b29d5816df4f0fa0d198290808aa6eca05d
Gabriel <gabrieltlaltilolpa@gmail.com>
2023-08-11 19:39:49 -0600

    Creando tabla

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  datasourceTable? : MenuDatos[];
+  displayColumns = ['ID','Nombre','Precio', 'Descripcion'];
+  constructor(){
 
+  }
+
+  ngOnInit(){
+    this.cargarTabla();
+  
+  }
+  cargarTabla(){
+    this.datasourceTable = [
+      {
+        idItem:1,
+        nombre:'Vaso',
+        precio:'10',
+        descripcion:'Vidrio'
+      },
+      {
+        idItem:2,
+        nombre:'Vaso',
+        precio:'10',
+        descripcion:'Vidrio'
+      }
+    ];
+  }
+}
+
+type MenuDatos = {
+  idItem?:number,
+  nombre?:string,
+  precio?:string,
+  descripcion?:string
 }
